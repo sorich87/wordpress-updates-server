1acee5a3f03108238d5d6a7cdfee3e4a9fb0c88e
Emil Ahlbäck <e.ahlback@gmail.com>
2012-05-24 13:19:37 +0300

    Factory for testing

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,21 @@
+def get_or_create_business
+  Business.first || FactoryGirl.create(:business)
+end
+
 FactoryGirl.define do
   factory :business do
     email   "factory@example.com"
     name    "FActory Company"
+  end
+
+  factory :package do
+    name            "Standard"
+    description     "Awesome"
+    price           40.00
+    themes          Package::THEMES[:one_theme]
+    domains         0
+    billing         Package::BILLING[:subscription]
+    validity        Package::VALIDITY[:one_month]
+    business        get_or_create_business
   end
 end

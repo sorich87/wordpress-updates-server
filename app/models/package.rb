878ea4c5757c2d3323caf0c0e2706e68788319f0
Emil Ahlbäck <e.ahlback@gmail.com>
2012-05-25 10:31:07 +0300

    Packages can be free, change validation to >= 0

--- app/models/package.rb
+++ app/models/package.rb
@@ -31,7 +31,7 @@ class Package
   validates_presence_of [:name, :description, :price, :themes, :domains, :billing, :validity, :business]
   
   validates_numericality_of :price, 
-    :greater_than => 0.00
+    :greater_or_equal_than => 0
   
   validates_inclusion_of :validity,
     :in => VALIDITY.values

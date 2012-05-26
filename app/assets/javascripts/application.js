3d8fbee4661cc95e3debcded73fb2faa20c7f472
Emil Ahlbäck <e.ahlback@gmail.com>
2012-05-26 13:40:27 +0300

    Get rid of javascript error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,17 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require bootstrap
+//= require bootstrap-tooltip
+//= require bootstrap-popover
+//= require bootstrap-alert
+//= require bootstrap-button
+//= require bootstrap-carousel
+//= require bootstrap-collapse
+//= require bootstrap-dropdown
+//= require bootstrap-modal
+//= require bootstrap-scrollspy
+//= require bootstrap-tab
+//= require bootstrap-tooltip
+//= require bootstrap-transition
+//= require bootstrap-typeahead

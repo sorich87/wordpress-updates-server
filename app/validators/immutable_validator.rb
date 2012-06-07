8caea8d391e6c35ee64f046ee3e0f0688e74ad59
Emil Ahlbäck <e.ahlback@gmail.com>
2012-06-07 11:56:38 +0300

    Clean up.

--- app/validators/immutable_validator.rb
+++ app/validators/immutable_validator.rb
@@ -1,6 +1,5 @@
 class ImmutableValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     record.errors.add(attribute, :immutable_attribute) if record.send("#{attribute}_changed?")
-    #record.errors[attribute] << "cannot be changed after creation" if record.send("#{attribute}_changed?")
   end
 end

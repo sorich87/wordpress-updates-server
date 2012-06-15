8cd8150b906e559a3c344f7efc80327a4878265e
Ulrich Sossou <sorich87@gmail.com>
2012-06-15 13:15:16 +0100

    Configured s3 for production

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 Paperclip::Attachment.default_options[:storage] = :fog
 Paperclip::Attachment.default_options[:fog_credentials] = File.join(Rails.root, 'config', 'fog.yml')
-Paperclip::Attachment.default_options[:fog_directory] = "thememy-test"
+Paperclip::Attachment.default_options[:fog_directory] = Rails.env.test? ? ENV['S3_BUCKET_NAME'] : "thememy-test"

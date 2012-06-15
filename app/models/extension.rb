f4d5876467ec2652baadb3c215a6c3c17822f10b
Emil Ahlbäck <e.ahlback@gmail.com>
2012-06-15 13:07:07 +0300

    Grab screenshot in Extension, not Theme

--- app/models/extension.rb
+++ app/models/extension.rb
@@ -55,4 +55,22 @@ class Extension
   def set_current_version
     self.current_version = self.versions.last.version
   end
+
+  def grab_screenshot_from_zip(value)
+    zip_file = value[:attachment].tempfile
+    screenshot_path_in_zip = value[:screenshot_path_in_zip]
+    return if screenshot_path_in_zip.nil?
+
+    zip = Zip::ZipFile.new(zip_file)
+    zip_entry = zip.get_entry(screenshot_path_in_zip)
+
+    filename = screenshot_path_in_zip.split('/')[-1]
+
+    tempname = File.join(Rails.root, 'tmp', "#{DateTime.now.to_i}-#{name}-#{filename}")
+    zip_entry.extract(tempname)
+
+    tempfile = File.new(tempname)
+    screenshot.assign( tempfile )
+    File.delete(tempname)
+  end
 end

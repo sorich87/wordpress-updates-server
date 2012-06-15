87bb450231f7f17c3c83e2e73b1af1630d03974d
Ulrich Sossou <sorich87@gmail.com>
2012-06-15 22:07:25 +0100

    Fixed site mailer spec

--- spec/mailers/site_mailer_spec.rb
+++ spec/mailers/site_mailer_spec.rb
@@ -2,10 +2,10 @@ require "spec_helper"
 
 describe SiteMailer do
   describe ".confirmation_instructions" do
-    subject { SiteMailer.confirmation_instructions(site) }
     let(:site) { FactoryGirl.create(:unconfirmed_site) }
-    it { should have_sent_email.with_subject(/Please Confirm Your Website$/) }
-    it { should have_sent_email.to(site.customer.email) }
-    it { should have_sent_email.with_body(/Confirm my website/) }
+    subject { SiteMailer.confirmation_instructions(site) }
+    it { should have_subject(/Please Confirm Your Website$/) }
+    it { should deliver_to(site.customer.email) }
+    it { should have_body_text(/Confirm my website/) }
   end
 end

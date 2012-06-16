adf62ccdf768cf41132a4c37a785a34156877dc5
Ulrich Sossou <sorich87@gmail.com>
2012-06-16 01:03:54 +0100

    Added "from" header to site confirmation email

--- app/mailers/site_mailer.rb
+++ app/mailers/site_mailer.rb
@@ -1,4 +1,6 @@
 class SiteMailer < ActionMailer::Base
+  default :from => "no-reply@push.ly"
+
   def confirmation_instructions(site)
     @site = site
     mail to: @site.customer.email, subject: "Please Confirm Your Website"
